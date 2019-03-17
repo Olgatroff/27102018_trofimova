d4214525866f1cc47a62605386994009fdc6be0e
olatroff <olatroff123@gmail.com>
2019-03-17 19:46:07 +0300

    Подключил header и footer

--- product/index.php
+++ product/index.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="hello">Привет еще раз</div>
-</body>
-</html>
+<?php 
+  $cssFiles = [
+    '/css/dist/catalog.css'
+  ];
+  include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
+?>
+
+<?php 
+  include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php');
+?>
